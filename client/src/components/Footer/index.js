f06ef1da9afa099d53f119b6779ab23f63ec635d
Omar Dahmash <omartdh@Omars-MacBook-Pro.local>
2022-01-15 18:20:57 -0800

    edited footer css

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -9,7 +9,7 @@ function Footer() {
         <h3>Cryptography</h3>
         <p>Designed and developed by:</p>
         <ul className="socials">
-            <li>Omar Dahmash<a href="https://github.com/omartdh"><i className="bx bxl-github"></i></a><a href="https://www.linkedin.com/in/omar-dahmash-b34693a4/"><i className="bx bxl-linkedin"></i></a></li>
+            <li>Omar Dhmash<a href="https://github.com/omartdh"><i className="bx bxl-github"></i></a><a href="https://www.linkedin.com/in/omar-dahmash-b34693a4/"><i className="bx bxl-linkedin"></i></a></li>
             <li>Michael Kahn<a href="https://github.com/mpkahn"><i className="bx bxl-github"></i></a><a href="https://www.linkedin.com/in/michael-kahn-9b011b8a/"><i className="bx bxl-linkedin"></i></a></li>
             <li>Miguel Alcantar<a href="https://github.com/malcan09"><i className="bx bxl-github"></i></a><a href="https://www.linkedin.com/in/miguel-alcantar-8018531b9/"><i className="bx bxl-linkedin"></i></a></li>
         </ul>
